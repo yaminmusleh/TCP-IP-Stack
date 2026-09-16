e2540b663e19062a69a31af6e34c96f19d7391bc
yaminmusleh <yaminaktham3@gmail.com>
2026-09-17 00:07:23 +0300

    the project now can understand and fetch arp frames (arp request detected)

--- build/CMakeFiles/tcp_stack.dir/src/main.cpp.o.d
+++ build/CMakeFiles/tcp_stack.dir/src/main.cpp.o.d
@@ -182,4 +182,5 @@ CMakeFiles/tcp_stack.dir/src/main.cpp.o: \
  /usr/include/c++/15/cstdint \
  /usr/lib/gcc/x86_64-linux-gnu/15/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-least.h
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /home/yamenmosleh/tcp-stack/include/net/arp.hpp
--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,50 @@
 
 #include "net/tap.hpp"
 #include "net/ethernet.hpp"
+#include "net/arp.hpp"
 
 int main()
 {
     TapDevice tap("tap0");
 
-    std::cout << "Waiting for an Ethernet frame...\n";
+    while (true)
+    {
+        std::cout << "Waiting for an Ethernet frame...\n";
 
-    auto frame = tap.readFrame();
+        auto raw_frame = tap.readFrame();
 
-    EthernetFrame ethernet = EthernetFrame::parse(frame);
+        EthernetFrame ethernet =
+            EthernetFrame::parse(raw_frame);
 
-    std::cout << "Received frame: "
-              << frame.size()
-              << " bytes\n";
+        std::cout << "EtherType: 0x"
+                  << std::hex
+                  << std::setw(4)
+                  << std::setfill('0')
+                  << ethernet.etherType()
+                  << std::dec
+                  << '\n';
 
-    std::cout << "EtherType: 0x"
-              << std::hex
-              << std::setw(4)
-              << std::setfill('0')
-              << ethernet.etherType()
-              << std::dec
-              << '\n';
+        if (ethernet.etherType() != 0x0806) // since we said the ARP Frame is 0x0806, anything else is not an ARP frame.
+        {
+            std::cout << "Not an ARP frame. Ignoring.\n";
+            continue;
+        }
 
-    std::cout<< "Payload size: "
-             <<ethernet.payload().size()
-             <<"bytes\n";
+        ArpPacket arp =
+            ArpPacket::parse(ethernet.payload());
+
+        std::cout << "ARP hardware type: "
+                  << arp.hardwareType()
+                  << '\n';
+
+        std::cout << "ARP protocol type: 0x"
+                  << std::hex
+                  << arp.protocolType()
+                  << std::dec
+                  << '\n';
+
+        std::cout << "ARP opcode: "
+                  << arp.opCode()
+                  << '\n';
+    }
 }
