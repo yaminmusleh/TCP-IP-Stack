8e8d7b290e0fe2c603670ae37efc367951032701
yaminmusleh <yaminaktham3@gmail.com>
2026-09-23 11:55:59 +0300

    ipv4.cpp: parsed total_length_ + identification_ + flags_ + fragment_offset_

--- src/net/ipv4.cpp
+++ src/net/ipv4.cpp
@@ -25,5 +25,19 @@ Ipv4Packet Ipv4Packet::parse(const std::vector<unsigned char> &data)
 
     packet.tos_ = data[1];
 
+    packet.total_length_ = (static_cast<std::uint16_t>(data[2]) << 8) | // the OR (|) sums the bits
+                           (static_cast<std::uint16_t>(data[3]));
+
+    packet.identification_ =
+        (static_cast<std::uint16_t>(data[4]) << 8) |
+        static_cast<std::uint16_t>(data[5]);
+
+    std::uint16_t flagsAndOffset = (static_cast<std::uint16_t>(data[6])) |
+                                   (static_cast<std::uint16_t>(data[7]));
+
+    packet.flags_ = static_cast<std::uint8_t>(flagsAndOffset >> 13);
+
+    packet.fragment_offset_ = flagsAndOffset & 0x1FFF;
+
     return packet;
 }
